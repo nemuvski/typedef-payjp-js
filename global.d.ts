236db8301b53e62bdd8fff02a5bbb638be359bd7
yuumillar <araera111@gmail.com>
2024-10-09 18:47:36 +0900

    feat(global.d.ts): PayjpのoptionsにthreeDSecureWorkflowを追加
    feat #1

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,16 @@
-import type PayjpJs from './index'
+import type PayjpJs from "./index";
 
 /**
  * windowオブジェクトからpayjp.jsのAPIを参照
  */
 declare global {
-  interface Window {
-    Payjp?: (publicKey: string, options?: { locale?: PayjpJs.Locale }) => PayjpJs.Payjp
-  }
+	interface Window {
+		Payjp?: (
+			publicKey: string,
+			options?: {
+				locale?: PayjpJs.Locale;
+				threeDSecureWorkflow: "subwindow" | "redirect";
+			},
+		) => PayjpJs.Payjp;
+	}
 }
