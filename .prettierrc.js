5e1df5f7b09eb688c99f1e9a849513c0cf04bfb1
yuumillar <araera111@gmail.com>
2024-10-09 19:01:09 +0900

    feat:

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	printWidth: 120,
-	tabWidth: 2,
-	singleQuote: true,
-	semi: false,
-	parser: "typescript",
-	jsxSingleQuote: true,
-	arrowParens: "always",
-	endOfLine: "lf",
-};
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  parser: 'typescript',
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
